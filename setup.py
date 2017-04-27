1986b5399e914a98aad0764ed5228392d230bbee
hparks <holdenparks@yahoo.com>
2017-04-27 09:21:52 -0700

    change version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='pyf3d',
-    version='1.0.0',
+    version='1.0.1',
     description='Filtering for micro-tomography data',
     long_description=long_description,
     url='https://github.com/holdymoldy/pyF3D',
